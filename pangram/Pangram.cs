b0ef74ad04b435bb9856b20e98b00dc241b439ec
Gordon Li <gordonli08@gmail.com>
2020-07-20 17:52:20 -0400

    pangram done

---
+++ pangram/Pangram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static bool IsPangram(string input)
+    {
+        return alphabet.All(input.ToLower().Contains);
+    }
+}
