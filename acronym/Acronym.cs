0bb7afad1d2b47cf85ebfd2e45671a3ba9c1ca60
Gordon Li <gordonli08@gmail.com>
2020-07-23 16:15:31 -0400

    Acronym

---
+++ acronym/Acronym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public static class Acronym
+{
+    private static readonly char[] delimiters = {' ', '-', '_'};
+
+    public static string Abbreviate(string phrase)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach(string word in phrase.ToUpper().Split(delimiters, StringSplitOptions.RemoveEmptyEntries)) {
+            sb.Append(word[0]);
+        }
+        return sb.ToString();
+    }
+}
