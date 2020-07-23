bb5fd4772f1fc9839b56fa41d8e7500e3b306225
Gordon Li <gordonli08@gmail.com>
2020-07-23 15:46:17 -0400

    Resistor Color var name change

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 public static class ResistorColor
 {
-    static readonly Dictionary<string, int> transistors = new Dictionary<string, int>{
+    static readonly Dictionary<string, int> resistors = new Dictionary<string, int>{
         {"black", 0},
         {"brown", 1},
         {"red", 2},
@@ -16,7 +16,7 @@ public static class ResistorColor
         {"grey", 8},
         {"white", 9},
     };
-    public static int ColorCode(string color) => transistors[color];
+    public static int ColorCode(string color) => resistors[color];
 
-    public static string[] Colors() => transistors.Keys.ToArray();
+    public static string[] Colors() => resistors.Keys.ToArray();
 }
