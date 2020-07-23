84206aa06d88192c6d683f19a173188b96eebd9c
Gordon Li <gordonli08@gmail.com>
2020-07-23 15:43:57 -0400

    Sum of Multiples

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int ret = 0;
+        for (int i = 1; i < max; i++) {
+            if (multiples.Any(fact => fact != 0 && i%fact == 0)) {
+                ret += i;
+            }
+        }
+        return ret;
+    }
+}
