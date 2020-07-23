ce903a4488a3ab54138f49eb4ee3a481c8f740e7
Gordon Li <gordonli08@gmail.com>
2020-07-23 16:30:55 -0400

    Collatz Conjecture

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0) {
+            throw new ArgumentOutOfRangeException();
+        }
+        int steps = 0;
+        while(number != 1) {
+            steps += 1;
+            number = collatz(number);
+        }
+        return steps;
+    }
+
+    private static int collatz(int number) {
+        int ret;
+        if (number%2 == 0) {
+            ret = number/2;
+        } else {
+            ret = 3*number + 1;
+        }
+        return ret;
+    }
+}
