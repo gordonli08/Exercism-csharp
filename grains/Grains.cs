3be2e515148b0befda76cc9f4c121bf91a6dd962
Gordon Li <gordonli08@gmail.com>
2020-07-24 14:15:33 -0400

    Grains

---
+++ grains/Grains.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64) {
+            throw new ArgumentOutOfRangeException();
+        }
+        return (ulong) Math.Pow(2,n-1);
+
+    }
+
+    public static ulong Total() =>
+        (ulong) Math.Pow(2,65) - 1;
+}
