10ab1b1f04a46d5ec27d1485b6b3a8bea9a65f39
Gordon Li <gordonli08@gmail.com>
2020-07-24 17:57:05 -0400

    Perfect Numbers

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) {
+            throw new ArgumentOutOfRangeException();
+        }
+        int maxfact = (int) Math.Sqrt(number);
+        int aliquot = 0;
+        for (int i = 1; i <= maxfact; i++) {
+            if (number%i == 0) {
+                if (i*i == number) {
+                    aliquot += i;
+                } else {
+                    aliquot += i + (number/i);
+                }
+            }
+        }
+        aliquot -= number;
+        if (aliquot > number) return Classification.Abundant;
+        if (aliquot < number) return Classification.Deficient;
+        return Classification.Perfect;
+    }
+}
